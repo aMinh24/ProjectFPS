25401d88a43b59c36251b99f5cb5e5f86a67aba2
Ho Anh Minh <yamchahuyvs@gmail.com>
2023-09-04 22:26:03 +0700

    [fix pistol]

--- Assets/Scripts/EnemyAi/AiAgent.cs
+++ Assets/Scripts/EnemyAi/AiAgent.cs
@@ -78,7 +78,6 @@ public class AiAgent : MonoBehaviour
 	// Token: 0x060000A1 RID: 161 RVA: 0x000055F0 File Offset: 0x000037F0
 	private void Update()
 	{
-		Debug.Log(stateMachine.currentState);
 		this.stateMachine.Update();
 	}
 
